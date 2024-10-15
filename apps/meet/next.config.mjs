2aebae654271b77dd7b95a096fb8b4191a2fe67a
Cao Havan <caohv.dev@gmail.com>
2024-10-15 21:26:30 +0700

    feat: change output

--- apps/meet/next.config.mjs
+++ apps/meet/next.config.mjs
@@ -1,7 +1,18 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: false,
-  transpilePackages: ['@atm0s-media-sdk/ui', '@atm0s-media-sdk/core', '@atm0s-media-sdk/react-hooks', '@atm0s-media-sdk/react-ui'],
+  transpilePackages: [
+    '@atm0s-media-sdk/ui',
+    '@atm0s-media-sdk/core',
+    '@atm0s-media-sdk/react-hooks',
+    '@atm0s-media-sdk/react-ui',
+  ],
+}
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
 }
 
 export default nextConfig
